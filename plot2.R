b45f84f3c34c6eecb874ebe23c62566ba1af451b
Vladimir Bondarevsky <vbondarevsky@gmail.com>
2015-05-10 20:08:44 +0300

    plot 2, 3, 4

---
+++ plot2.R
@@ -0,0 +1,22 @@
+#assume the database file located at working directory
+
+colNames = c("Date", "Time", "Global_active_power", "Global_reactive_power",
+             "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", 
+             "Sub_metering_3")
+colClasses = c("character", "character", rep("numeric", 7) )
+
+#read file
+df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                 col.names = colNames, colClasses = colClasses, na.strings = "?")
+
+#convert to Date type, then filter
+df$Date = as.Date(df$Date, format = "%d/%m/%Y")
+df = df[df$Date >= as.Date("2007-02-01") & df$Date <= as.Date("2007-02-02"),]
+
+#plot and save graph
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(df$Global_active_power, type = "l", xaxt = "n", xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+axis(1, at = c(1, as.integer(nrow(df) / 2), nrow(df)),
+     labels = c("Thu", "Fri", "Sat"))
+dev.off()
